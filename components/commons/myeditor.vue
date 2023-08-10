4a2555eb55474db123b9c45769f8aef2ad7c5a77
coder <coder@email.com>
2023-08-11 07:08:19 +0900

    validation Form 컴포넌트 대신 useForm 사용하도록
    1. validation Form -> useForm 으로 변경
    2. 하위컴포넌트 및 화면 컴포넌트에서 form 참조 가능하도록 수정 (vue 객체 탐색)

--- components/commons/myeditor.vue
+++ components/commons/myeditor.vue
@@ -2,7 +2,7 @@
   <editor-content
     :name="props.name"
     v-bind="attrs"
-    @keydown="emitUpdate"
+    @keyup="emitUpdate"
     :editor="editor"
     />
   <span class="err-msg" v-if="vmeta.validated && errorMessage" v-html="errorMessage"></span>
@@ -10,23 +10,25 @@
 <script setup lang="ts">
 import * as C from '@/libs/commons/constants'
 import { log } from '@/libs/commons/log'
+import { inst } from '@/libs/commons/shared'
 import { useField } from 'vee-validate';
 import { useEditor, EditorContent } from '@tiptap/vue-3'
 import StarterKit from '@tiptap/starter-kit'
 
 import lodash from 'lodash'
 
+const self = inst(getCurrentInstance())
+
 const props = defineProps({
   label: String,
   name: String,
   validrules: {} as any,
-  validopts: {} as any,
   modelValue: String
 })
 
 const attrs = useAttrs()
 const emit = defineEmits([C.UPDATE_MODEL_VALUE])
-const vfield = useField(() => props.name || '', props.validrules, props.validopts)
+const vfield = useField(() => props.name || '', props.validrules, { label: ref(props.label) })
 const { value, errorMessage } = vfield
 const vmeta: any = vfield.meta
 const { debounce } = lodash
@@ -36,12 +38,10 @@ const editor = useEditor({
   extensions: [StarterKit],
 })
 
-onBeforeMount(async () => {
-  vmeta.label = props.label
-})
-
 watch(() => props.modelValue, (v: any) => {
   editor.value?.commands?.setContent(v)
+  value.value = v
+  vfield.meta.validated = false
 })
 
 const emitUpdate = debounce(async (e?: any) => {
