1cde44d948539396746a0ff9847f6fc9548531f7
coder <coder@email.com>
2023-08-12 21:30:34 +0900

    클라이언트와 REST 통신, 세션에 로그인정보 저장 가능하도록 수정.

--- src/main/java/my/was/mywas/user/UserController.java
+++ src/main/java/my/was/mywas/user/UserController.java
@@ -1,6 +1,9 @@
 package my.was.mywas.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -8,8 +11,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+
 import static my.was.mywas.util.Constants.*;
 
+import java.util.Iterator;
+
+@Slf4j
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
@@ -21,8 +30,19 @@ public class UserController {
     return service.get(id);
   }
 
+  @Autowired
+  AuthenticationManager authman;
+
   @GetMapping("/check/{id}")
-  public Object check(@PathVariable String id) {
+  public Object check(@PathVariable String id, HttpSession session) {
+
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    log.debug("SESSION-ID:{} / {} / {}", session.getId(), authman, auth);
+    Iterator<String> iter = session.getAttributeNames().asIterator();
+    while (iter.hasNext()) {
+      String key = iter.next();
+      log.debug("SESSION-KEY:{} / {}", key, session.getAttribute(key));
+    }
     return service.check(id);
   }
 
--- src/main/java/my/was/mywas/user/UserService.java
+++ src/main/java/my/was/mywas/user/UserService.java
@@ -1,19 +1,33 @@
 package my.was.mywas.user;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
+
 import static my.was.mywas.util.DateConverter.dateToStr;
 
+@Slf4j
 @Service
 public class UserService {
   @Autowired private UserRepository repository;
 
   public User get(String id) {
     return repository.findById(Long.parseLong(id)).get();
+  }
+
+  public User getByUserId(String userId) {
+    List<User> users = repository.findByUserId(userId);
+    log.debug("USERS:{} / {}", userId, users);
+    if (users.size() > 0) {
+      return users.get(0);
+    } else {
+      return null;
+    }
   }
 
   public Object check(String userId) {
