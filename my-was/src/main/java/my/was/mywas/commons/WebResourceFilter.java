d9d0afff28e298f5c684d01e6657226398e844bc
lupfeliz <lupfeliz@gmail.com>
2024-09-03 11:24:32 +0900

    SPA 용 리소스 필터 완료

--- my-was/src/main/java/my/was/mywas/commons/WebResourceFilter.java
+++ my-was/src/main/java/my/was/mywas/commons/WebResourceFilter.java
@@ -7,13 +7,13 @@ import static com.ntiple.commons.ConvertUtil.cat;
 import static com.ntiple.commons.IOUtils.istream;
 import static com.ntiple.commons.IOUtils.passthrough;
 import static com.ntiple.commons.IOUtils.safeclose;
-import static org.springframework.http.HttpMethod.GET;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
+import java.util.regex.Pattern;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
@@ -24,17 +24,18 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpServletResponseWrapper;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
 public class WebResourceFilter extends GenericFilterBean {
+
+  private static Pattern ptn = Pattern.compile("\\/[^.^\\/]+$");
+
   @Override public void doFilter(ServletRequest sreq, ServletResponse sres, FilterChain chain)
     throws IOException, ServletException {
     HttpServletRequest req = (HttpServletRequest) sreq;
     HttpServletResponse res = (HttpServletResponse) sres;
-    ResponseWrap wres = new ResponseWrap(req, res);
     File base = null;
     File file = null;
     String uri = req.getRequestURI();
@@ -43,15 +44,20 @@ public class WebResourceFilter extends GenericFilterBean {
       base = new File(resroot.getFile());
       file = new File(base, uri);
     } catch (Exception ignore) { }
-    if (base == null || uri.startsWith("/api/")) {
-      chain.doFilter(sreq, wres);
+    if (
+      base == null ||
+      uri.startsWith("/api/") ||
+      uri.startsWith("/_next/") ||
+      (file != null && file.exists()) ||
+      !ptn.matcher(uri).find()
+      ) {
+      chain.doFilter(sreq, res);
       return;
     } else {
       if (file != null && !file.exists()) { file = new File(base, cat(uri, ".html")); }
       if (file != null && !file.exists()) { file = new File(base, "index.html"); }
       log.debug("FILTER-URI:{} {}", uri, file);
-      // chain.doFilter(sreq, wres);
-      // if (Integer.valueOf(404).equals(req.getAttribute("STAT_CODE"))) {
+      if (file != null && file.exists()) {
         InputStream istream = null;
         OutputStream ostream = null;
         try {
@@ -69,24 +75,9 @@ public class WebResourceFilter extends GenericFilterBean {
           safeclose(istream);
           safeclose(ostream);
         }
-      // }
-    }
-  }
-
-  static class ResponseWrap extends HttpServletResponseWrapper {
-    // extends HttpResponseAdopter
-    HttpServletRequest req;
-    public ResponseWrap(HttpServletRequest req, HttpServletResponse res) {
-      super(res);
-      this.req = req;
-    }
-    @Override public void sendError(int sc, String msg) throws IOException {
-      if (sc == 404 && GET.name().equals(req.getMethod())) {
-        req.setAttribute("STAT_CODE", sc);
       } else {
-        super.sendError(sc, msg);
+        chain.doFilter(sreq, res);
       }
     }
   }
 }
-
