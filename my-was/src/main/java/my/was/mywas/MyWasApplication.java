c2298cbe2e7ba96489be793903f632a00ca21223
lupfeliz <lupfeliz@gmail.com>
2024-09-03 09:54:06 +0900

    was 변동사항 반영

--- my-was/src/main/java/my/was/mywas/MyWasApplication.java
+++ my-was/src/main/java/my/was/mywas/MyWasApplication.java
@@ -1,3 +1,10 @@
+/**
+ * @File        : MyWasApplication.java
+ * @Author      : 정재백
+ * @Since       : 2024-04-16 
+ * @Description : Springboot 구동 파일
+ * @Site        : https://devlog.ntiple.com/795
+ **/
 package my.was.mywas;
 
 import org.springframework.boot.SpringApplication;
@@ -6,6 +13,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyWasApplication {
   public static void main(String[] args) {
-  	SpringApplication.run(MyWasApplication.class, args);
+    String profile = System.getProperty("spring.profiles.active");
+    if (profile == null || "".equals(profile)) {
+      System.setProperty("spring.profiles.active", "local");
+    }
+    SpringApplication.run(MyWasApplication.class, args);
   }
 }
