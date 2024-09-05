bcbfafa77138f5e30cc150a35f8800241c95b893
lupfeliz <lupfeliz@gmail.com>
2024-09-05 23:56:11 +0900

    모바일 환경에서의 footer 이벤트 핸들링

--- my-next-app/src/components/footer.tsx
+++ my-next-app/src/components/footer.tsx
@@ -8,41 +8,70 @@
 import $ from 'jquery'
 import lodash from 'lodash'
 import app from '@/libs/app-context'
+import * as C from '@/libs/constants'
 import { Container } from '@/components'
 
-const { defineComponent, useSetup, useRef } = app
+const { defineComponent, useSetup, useRef, log } = app
 const { debounce } = lodash
 const evtlst = ['scroll', 'resize']
 export default defineComponent(() => {
-  const eref = useRef({} as HTMLDivElement)
-  useSetup({
+  const self = useSetup({
     name: 'footer',
+    vars: {
+      hndTouch: C.UNDEFINED,
+      elem: useRef({} as HTMLDivElement),
+      isScrolling: false
+    },
     async mounted({ releaser }) {
       evtlst.map(v => window.addEventListener(v, fncResize))
       fncResize()
+      document.addEventListener('touchstart', fncHide)
+      document.addEventListener('touchend', fncCancelHide)
+      vars.elem.current.addEventListener('touchstart', fncHide)
     },
     async unmount() {
       evtlst.map(v => window.removeEventListener(v, fncResize))
+      document.removeEventListener('touchstart', fncHide)
+      document.removeEventListener('touchend', fncCancelHide)
+      vars.elem.current.removeEventListener('touchstart', fncHide)
     }
   })
+  const { vars } = self()
   const fncResizePost = debounce(() => {
     const page = $('html,body')
     /** footer sticky 기능을 위해 css 변수를 수정한다 */
-    const footer = eref.current || {}
+    const footer = vars.elem?.current || {}
     {[
       '--screen-height', `${window.innerHeight || 0}px`,
       '--scroll-top', `${page.scrollTop()}px`,
       '--footer-height', `${footer?.offsetHeight || 0}px`
     ].map((v, i, l) => (i % 2) &&
       footer?.style?.setProperty(l[i - 1], v))}
-    footer?.classList?.remove('hide')
-  }, 100)
+    if (!vars.isScrolling) {
+      footer?.classList?.remove('hide')
+    }
+  }, 50)
+  const fncHide = (e: any) => {
+    if (e?.target === vars.elem.current || vars.elem.current.contains(e?.target)) {
+      log.debug('FNC-HIDE:', e)
+    } else {
+      vars.hndTouch = setTimeout(() => {
+        vars.isScrolling = true 
+        vars.elem?.current?.classList?.add('sticky', 'hide')
+      }, 100)
+    }
+  }
+  const fncCancelHide = () => {
+    if (vars.hndTouch) { clearTimeout(vars.hndTouch) }
+    vars.elem?.current?.classList?.remove('hide')
+    vars.isScrolling = false
+  }
   const fncResize = () => {
-    eref?.current?.classList?.add('sticky', 'hide')
+    vars.elem?.current?.classList?.add('sticky', 'hide')
     fncResizePost()
   }
   return (
-    <footer ref={ eref }>
+    <footer ref={ vars.elem }>
     <Container>
       FOOTER
     </Container>
