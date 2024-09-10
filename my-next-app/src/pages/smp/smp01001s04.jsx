6994dc3f5ed5ca932217e452107120498fcc0946
lupfeliz <lupfeliz@gmail.com>
2024-09-11 01:51:33 +0900

    public-key encrypt / decrypt 테스트 (JSEncrypt)

--- my-next-app/src/pages/smp/smp01001s04.jsx
+++ my-next-app/src/pages/smp/smp01001s04.jsx
@@ -12,8 +12,11 @@ import crypto from '@/libs/crypto'
 import * as C from '@/libs/constants'
 import { Block, Form, Button, Input, Select, Container, Textarea } from '@/components'
 // import NodeRSA from 'encrypt-rsa'
-import { KEYUTIL, RSAKey, KJUR } from 'jsrsasign'
+// import { KEYUTIL, RSAKey, KJUR } from 'jsrsasign'
 // import {  } from 'jsrsasign-util'
+// import cryptojs from 'crypto-js'
+// import rsa from 'js-crypto-rsa'
+// import NodeRSA from 'node-rsa'
 
 const { definePage, useSetup, log, goPage, clone, matcher } = app
 const userInfo = userContext.getUserInfo()
@@ -48,14 +51,62 @@ export default definePage(() => {
         8GOet/eHsNQyJYd+FwJAPDgogfNfjBV6bQT2UQwHdfzKG1Jfcs5Pz/fLMljsa67I
         osWRyvZU4dRMwmNpo+m9YyKHDuQ/NwwMBhQtYlkzDw==
         -----END RSA PRIVATE KEY-----`;
+        {
+          let sig, enc, dec, pln
+          pln = '테스트'
+          const JSEncrypt = (await import('jsencrypt')).default
+          const encrypt = new JSEncrypt()
+          encrypt.setPublicKey('MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDIX1T+dD3zGK8umLDGKRHUz41kR35b1DQ4c4a4MjuYBIkyI9pSukveV0YILFPzRk20akaLgZhZVePihy/SyyoV7M6pQ0Q1n9Y2SQTXSOPxqUZUFPzJAco4vtpb/fPeICT16RMQyzv2zPj02dehzADMmSegzcOYQ/xjBLnWZOlNZQIDAQAB')
+          enc = encrypt.encrypt(pln)
+          log.debug('ENC:', enc)
+          encrypt.setPrivateKey('MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAMhfVP50PfMYry6YsMYpEdTPjWRHflvUNDhzhrgyO5gEiTIj2lK6S95XRggsU/NGTbRqRouBmFlV4+KHL9LLKhXszqlDRDWf1jZJBNdI4/GpRlQU/MkByji+2lv9894gJPXpExDLO/bM+PTZ16HMAMyZJ6DNw5hD/GMEudZk6U1lAgMBAAECgYAD/17nOrN3s6DfGZ3BPlWEPOXRv9lmBJxMGgXwi9QDiuefz/ZNmzjjRTN4+0Vrf5YSSOKCawH6mkuTG+ZY2sPKpiIBSX4SXewMIKRhbxOj19iwrNp1gBDK3s/kpHy9+x8b7tuEIITNYreuadYAvVgSMlaJVPdh7uUm39sMnn/aqwJBANRWQBpaOfw3iQ+g7jBrBJz+e5QbmsNZeNUPrk9WbMuhKQkLUP2E4U5noFKV8SZFGo48YPKsC32DxFF3e8kw2fsCQQDxk0ALLlepzdpqVkm6YQ4KtTEweug+L/RdjU5apGZLZI+AGfYIE1JIzyzgdPke92ePnw1wPqFgi0PyzyJn8DgfAkAq5M2IRUfHapSWgqT7RPMmn8XpEnZ+Ffnx2HwW7NeHfyPh/tY6kHhPNWHOrRmM6JLHvuy6uQSNM2waJO/toZ+3AkEAo4WzUl46RNztPhHOsnTEFod0Fob78ixv02u1YDHsdJhLcsEgA3NgvZxPmlhT0ZxS46scY6BhiIJ8qj1/4q9+rQJAbZj4vsQB5SrKcYNR8VVYMP9EYXnFax66QgSjGI3zPJhjAcbdyhDVshFKHcsI3R1dxegLFTbvjYKejzWfHBcBUQ==')
+          dec = encrypt.decrypt(enc)
+          log.debug('DEC:', dec)
+        }
+        {
+        }
 
-        const kpair = KEYUTIL.generateKeypair('RSA', 1024)
-        log.debug('PAIR:',  kpair)
+        // {
+        //   const crypto = require('crypto')
+        //   const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
+        //     modulusLength: 1024,
+        //     publicKeyEncoding: {
+        //       type: 'spki',
+        //       format: 'pem'
+        //     },
+        //     privateKeyEncoding: {
+        //       type: 'pkcs8',
+        //       format: 'pem'
+        //     }
+        //   })
+        //   log.debug('Public Key:', publicKey)
+        //   log.debug('Private Key:', privateKey)
+        // }
+        // {
+        //   let sig, enc, dec, pln
+        //   pln = '테스트'
+        //   sig = new KJUR.crypto.Signature({ 'alg': 'SHA1withRSA' })
+        //   sig.init(privateKey)
+        //   sig.updateString(pln)
+        //   enc = sig.sign()
+        //   log.debug('ENC:', enc)
+        // }
+        // {
+        //   // let sig, enc, dec, pln
+        //   // pln = '테스트'
+        //   // let rsa = new RSAKey()
+        //   // rsa.readPKCS8PubKeyHex()
+        //   // enc = rsa.encrypt(pln)
+        //   // log.debug('ENC:', enc)
+        // }
 
-        var rsa = new RSAKey()
-        rsa.readPrivateKeyFromPEMString(privateKey)
-        var hSig = rsa.signString('aaa', 'sha1')
-        log.debug('SIGN:', hSig)
+        // const kpair = KEYUTIL.generateKeypair('RSA', 1024)
+        // log.debug('PAIR:',  kpair)
+
+        // var rsa = new RSAKey()
+        // rsa.readPrivateKeyFromPEMString(privateKey)
+        // var hSig = rsa.signString('aaa', 'sha1')
+        // log.debug('SIGN:', hSig)
 
         // const nodeRSA = new NodeRSA(publicKey, privateKey, 2048);
         // var rsa = new RSAKey();
